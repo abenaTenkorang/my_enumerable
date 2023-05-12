b476f2ab7a434a9550f081b64c3207b995568c3a
abenaTenkorang <abenagy16@gmail.com>
2023-05-12 08:46:39 +0000

    Create my_list.rb that implements the methods

---
+++ my_list.rb
@@ -0,0 +1,27 @@
+require_relative 'enumeral'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*args)
+    @list = args
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 }) # true
+puts(list.all? { |e| e > 5 }) # false
+
+# Test #any?
+puts(list.any? { |e| e == 2 }) # true
+puts(list.any? { |e| e == 5 }) # false
+
+# Test #filter
+putslist.filter(&:even?) # [2, 4]
